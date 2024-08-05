c5a7e5c574d612f27c567dd237fab0c20b6e8ce1
ruksan_emirali <ruksanemirali@gmail.com>
2024-08-05 15:59:53 +0100

    creates 1 unit test

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -5,13 +5,25 @@ using NUnit.Framework;
 namespace GildedRoseTests;
 
 public class GildedRoseTest
+// {
+//     [Test]
+//     public void Foo()
+//     {
+//         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+//         var app = new GildedRose(items);
+//         app.UpdateQuality();
+//         Assert.That(items[0].Name, Is.EqualTo("fixme"));
+//     }
+// }
+
 {
     [Test]
-    public void Foo()
+    public void CheckSellInReduces()
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var itemSellIn = 10;
+        var items = new List<Item> { new Item { Name = "genericItem", SellIn = itemSellIn, Quality = 10 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].SellIn, Is.EqualTo(itemSellIn-1));
     }
 }
